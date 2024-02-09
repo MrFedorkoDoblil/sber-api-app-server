ba7f92c737b3e9fd759cd923429be94884138b26
mrFedorko <86523340+mrFedorko@users.noreply.github.com>
2024-02-09 14:52:09 +0300

    auth guard fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { AccountModule } from './account/account.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:example@mongo:27017/'),
+    MongooseModule.forRoot('mongodb://root:example@localhost:27018/'),
     ConfigModule.forRoot({
       envFilePath: '../.env',
       isGlobal: true,
