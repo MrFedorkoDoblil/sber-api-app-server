30a63499e3b8a9c3f3a9d5744658152c55c5c0d2
mrFedorko <mr_fedorko@mail.ru>
2024-02-15 13:25:55 +0300

    create path parser

--- libs/sbbApiEndpoints.ts
+++ libs/sbbApiEndpoints.ts
@@ -2,13 +2,13 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export type PathTree = {
-    [index: string]: {
+    [index :  string]: {
         url:string;
         children?: PathTree[]
     }
 }
 
-export const sbbAuthTree: PathTree = {
+export const sbbAuthTree = {
     base: {
         url: process.env.SB_ID_BASE_URL,
         children:[
@@ -33,13 +33,12 @@ export const sbbAuthTree: PathTree = {
                         }
                     ]
                 },
-
             },
         ]
     }
-}
+} 
 
-export const sbbFintechTree: PathTree = {
+export const sbbFintechTree = {
     base: {
         url: process.env.SB_FINTECH_BASE_URL,
         children: [
@@ -51,3 +50,47 @@ export const sbbFintechTree: PathTree = {
         ]
     }
 }
+
+type HandleChain<T extends string> = T extends "" ? "" : `.${T}`
+
+export type ExtractKeys<T extends PathTree, K extends keyof T> = 
+    {[P in keyof T[K]['children'][number]]: ExtractKeys<T[K]['children'][number], P>}
+
+export type PathKeys<T> = T extends object ? { [K in keyof T]:
+    `${Exclude<K, symbol>}${"" | `.${PathKeys<T[K]>}`}`
+  }[keyof T] : never
+
+export function getUrl<
+    T extends PathTree, 
+    K extends keyof T>(
+        tree: T, 
+        str: PathKeys<ExtractKeys<T, K>>, 
+        ) {
+    if(!str || !tree.base.url) return ''
+    const points = str.split('.')
+    const resultArray = [tree.base.url,]
+    let current = tree.base
+
+    points.forEach((item) => {
+        if(current?.children){
+            const hasItem = current.children.find(child => child[item]);
+            if(hasItem) {
+                current = hasItem[item]
+                resultArray.push(current.url);
+            }
+        } else{
+            resultArray.push (current[item]?.url)
+        }
+    })
+    return resultArray.join('')
+} 
+
+
+
+
+
+
+
+
+
+
